e36ec7ff318263546dec9524d312a0b9f6e3bba2
terrislost <terrislost>
2024-01-22 14:48:37 -0500

    commit did I git

---
+++ c/basics.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+
+	printf("this is my c basics file\n");
+	int num;
+	printf("how number is you: ");
+	scanf("%i", &num);
+	printf("number was Your%i\n", num);
+	return 0;
+}
